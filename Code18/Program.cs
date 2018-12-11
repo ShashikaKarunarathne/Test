aeb53ce1b29ba3438dd9095d8996b98224f9b2cd
Shashika Karunarathne <shahsika.lakmali@gmail.com>
2018-12-11 13:17:56 +0530

    First Commit

---
+++ Code18/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tutorial tute = new Tutorial();
+            tute.SetTute(1 ,"Software Engineering");
+            Console.Write(tute.GetID());
+            Console.Write(tute.GetTute());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Code18/Tutorial.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code18
+{
+    class Tutorial
+    {
+        public int TId;
+        public string TName;
+
+        public void SetTute(int MId,string MName)
+        {
+            TId = MId;
+            TName = MName;
+        }
+
+        public string GetTute()
+        {
+            return  TName;
+        }
+        public int GetID()
+        {
+            return TId;
+        }
+
+    }
+}
